9fdc52ee70355363ef09155c87160bb7e3c0a89e
Jennifer Young <jclyoung@gmail.com>
2019-06-19 15:12:03 -0500

    Finished

--- CarClass/Program.cs
+++ CarClass/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CarClass
-{
-    class Program
+{  
+
+    class Car
     {
-        static void Main(string[] args)
+
+        public float GasLevel { get; set; } 
+        public float FillUpAmount { get; set; }
+        public float NewGasLevel { get; set; }
+        public float MaxGas = 13;
+
+        public Car()
         {
-            Console.WriteLine("Hello World!");
+            GasLevel = RandomGasLevel();
+            FillUpAmount = FillUp();
+            NewGasLevel = AddGas(5);
+
+
+            float RandomGasLevel()
+            {
+                float randomGasLevel = new Random().Next(0, 13);
+                float GasLevel = randomGasLevel;
+                return GasLevel;
+            }
+
+
+            float FillUp()
+            {
+                float fillupamount = MaxGas - GasLevel;
+                float FillUpAmount = fillupamount;
+                return FillUpAmount;
+            }
+
+            float AddGas(float gasadded)
+            {
+
+                float newGasLevel = GasLevel + gasadded;
+                float NewGasLevel = newGasLevel;
+                return NewGasLevel;
+
+            }
+        
+        }        
+    }
+    
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+
         }
     }
 }
